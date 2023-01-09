40898e8f20dff6da24e5ead92f48011a28919281
Andrey <21elektro87@gmail.com>
2023-01-09 21:30:30 +0600

    add readonly_fields

--- core/admin.py
+++ core/admin.py
@@ -9,4 +9,5 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
-    exclude = ('password')
+    exclude = ('password',)
+    readonly_fields = ('last_login', 'date_joined')
